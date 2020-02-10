1ccb1e997fd2fe29b7c1c2755d738d466c9db7cb
江斌 <sudojs@gmail.com>
2020-02-10 12:30:22 +0800

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot and sentry 集成
---
+++ src/main/java/com/ipkaq/springbootsentry/demo/SpringbootSentryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ipkaq.springbootsentry.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSentryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSentryDemoApplication.class, args);
+    }
+
+}
